36f724f07ac21fb801607a0ad659e3a791f058a2
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-05-29 14:32:22 +0530

    added skills image

--- src/data.js
+++ src/data.js
@@ -4,34 +4,49 @@ import { CgFileDocument, CgList } from 'react-icons/cg';
 import { RiSendPlaneFill } from 'react-icons/ri';
 import { FiGithub } from 'react-icons/fi';
 import { SlSocialLinkedin } from 'react-icons/sl';
+import html from './image/html.svg';
+import css from './image/css.svg';
+import javaScript from './image/javascript.svg';
+import react from './image/react.svg';
+import nodejs from './image/nodejs.png';
+import bootStrap from './image/bootstrap.png';
 
-export const navBarList = [
+export const navbarList = [
   {
     id: 1,
     icon: <BiHome />,
-    iconName: 'Home',
+    name: 'Home',
     link: '#home',
   },
   {
     id: 2,
     icon: <RxPerson />,
-    iconName: 'About',
+    name: 'About',
   },
   {
     id: 3,
     icon: <CgFileDocument />,
-    iconName: 'Skill',
+    name: 'Skill',
   },
   {
     id: 4,
     icon: <CgList />,
-    iconName: 'Project',
+    name: 'Project',
   },
   {
     id: 5,
     icon: <RiSendPlaneFill />,
-    iconName: 'Contact Me',
+    name: 'Contact Me',
   },
 ];
 
 export const socialMediaIcon = [<FiGithub />, <SlSocialLinkedin />];
+
+export const skillsImage = [
+  { id: 1, img: html, name: 'html' },
+  { id: 2, img: css, name: 'css' },
+  { id: 3, img: javaScript, name: 'javascript' },
+  { id: 4, img: react, name: 'react js' },
+  { id: 5, img: nodejs, name: 'node js' },
+  { id: 6, img: bootStrap, name: 'bootstrap' },
+];
