dd2777e822cd5293a40dde7eaa1d235e1bed7f8f
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-06-17 20:37:25 +0530

    add toggle mode switch and menu button

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,36 +4,58 @@ import { navbarList } from '../data';
 import { BsMoon, BsXLg } from 'react-icons/bs';
 import { ImSun } from 'react-icons/im';
 import { BiGridAlt } from 'react-icons/bi';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isChangeButton, setIsChangeButton] = useState(true);
+  const [isMenuButton, setIsMenuButton] = useState(false);
+
+  const changeMode = () => {
+    setIsChangeButton((isChangeButton) => !isChangeButton);
+  };
+
+  const darkMode = () => {
+    document.querySelector('body').setAttribute('class', 'dark-theme');
+    return <ImSun />;
+  };
+
+  const lightMode = () => {
+    document.querySelector('body').setAttribute('class', 'light-theme');
+    return <BsMoon />;
+  };
+
+  const changeMenuBtn = () => {
+    setIsMenuButton((isMenuButton) => !isMenuButton);
+  };
+
   return (
     <nav>
       <div className='navbar'>
         <h1 className='logo'>RajkumarRaja</h1>
         <div className='menu'>
-          <ul className='navbar-list'>
+          <ul
+            className={`${
+              isMenuButton ? 'navbar-list active-navbar-list' : 'navbar-list'
+            }`}
+          >
             {navbarList.map((data) => {
               const { icon, href, name, id } = data;
               return (
-                <>
-                  <li key={id}>
-                    <a href={href} className='nav-menu'>
-                      <span className='nav-icon'>{icon}</span>
-                      <span className='nav-icon-name'>{name}</span>
-                    </a>
-                  </li>
-                </>
+                <li key={id} onClick={changeMenuBtn}>
+                  <a href={href} className='nav-menu'>
+                    <span className='nav-icon'>{icon}</span>
+                    <span className='nav-icon-name'>{name}</span>
+                  </a>
+                </li>
               );
             })}
           </ul>
           <div className='menuBtn'>
-            <button type='button' className='darkLight'>
-              {/* <BsMoon /> */}
-              <ImSun />
+            <button type='button' className='darkLight' onClick={changeMode}>
+              {isChangeButton ? darkMode() : lightMode()}
             </button>
-            <button type='button' className='menuIcon'>
-              <BiGridAlt />
-              {/* <BsXLg /> */}
+            <button type='button' className='menuIcon' onClick={changeMenuBtn}>
+              {isMenuButton ? <BsXLg /> : <BiGridAlt />}
             </button>
           </div>
         </div>
