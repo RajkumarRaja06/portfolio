65239345b0aa0d712d5f330645811c490d26c9bb
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-06-17 20:30:39 +0530

    add footer title

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,23 +1,26 @@
-import Title from './Title';
 import { socialMediaIcon, footerLinks } from '../data';
 import '../style/footer.css';
+import '../style/index.css';
 
 const Footer = () => {
   return (
     <footer>
       <div className='footer-data'>
-        <Title name='Web Developer' subtitle='Web Developer' />
+        <div className='title-container'>
+          <h2 className='titleName'>Rajkumar</h2>
+          <span className='titleSubtitle'>Web Developer</span>
+        </div>
         <div className='footer-nav-link'>
-          {footerLinks.map((link) => {
+          {footerLinks.map((link, index) => {
             const { name, href } = link;
             return (
-              <a href={href} className='footer-icon'>
+              <a href={href} className='footer-icon' key={index}>
                 {name}
               </a>
             );
           })}
         </div>
-        <div className='social-media-icon'>
+        <div className='social'>
           {socialMediaIcon.map(({ id, icon, link }) => {
             return (
               <a href={link} key={id}>
