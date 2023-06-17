b0623864ba4cb1375a47fd77ef712dbef30c13ec
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-06-17 20:30:09 +0530

    add action in form

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,30 +2,54 @@ import '../style/index.css';
 import Title from './Title';
 import '../style/contact.css';
 import { AiOutlineSend } from 'react-icons/ai';
+import { useState } from 'react';
 
 const Contact = () => {
+  // const [users, setUsers] = useState({ name: '', email: '', message: '' });
+
+  // function handleOnchange(event) {
+  //   setUsers({
+  //     ...users,
+  //     [event.target.name]: '',
+  //   });
+  // }
+
   return (
     <div className='contact' id='contact'>
       <Title name='Contact Me' subtitle='Get in touch' />
-      <form className='contact-form'>
+      <form
+        className='contact-form'
+        action='https://formsubmit.co/rkrajkumarco@gmail.com'
+        method='POST'
+      >
         <input
           type='text'
+          name='name'
           placeholder='Enter your Name'
           className='contact-form-name'
+          required
+          // onSubmit={(event) => handleOnchange(event)}
         />
+
         <input
           type='email'
+          name='email'
           placeholder='Enter a valid email address'
           className='contact-form-email'
+          required
+          // onSubmit={(event) => handleOnchange(event)}
         />
+
         <textarea
           name='message'
-          id=''
           cols='30'
           rows='10'
           className='contact-form-message'
           placeholder='Message'
+          required
+          // onSubmit={(event) => handleOnchange(event)}
         ></textarea>
+
         <div className='btn-container'>
           <button type='submit' className='btn connect'>
             Send Message
